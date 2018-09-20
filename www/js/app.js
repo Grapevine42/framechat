f381a72aa3d6348f95c0224e99787cfda1596751
Grapevine <jinsw2001@gmail.com>
2018-09-20 11:04:33 +0900

    탭 순서 변경

--- www/js/app.js
+++ www/js/app.js
@@ -31,11 +31,11 @@ var app  = new Framework7({
 var homeView = app.views.create('#view-home', {
   url: '/'
 });
-var catalogView = app.views.create('#view-catalog', {
-  url: '/catalog/'
+var chatView = app.views.create('#view-chat', {
+  url: '/chat/'
 });
-var settingsView = app.views.create('#view-settings', {
-  url: '/settings/'
+var settingView = app.views.create('#view-setting', {
+  url: '/setting/'
 });
 
 
